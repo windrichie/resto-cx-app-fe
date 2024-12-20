d690f8f30addd849c6e7ab2a2deb1ac6290dd812
Windrich <windrich@alvaenergi.com>
2024-12-20 16:31:47 +0800

    added default vercel domain

--- src/lib/utils/common.ts
+++ src/lib/utils/common.ts
@@ -1,6 +1,6 @@
 export function getBaseUrl() {
     if (process.env.NODE_ENV === 'production') {
-        return process.env.NEXT_PUBLIC_BASE_URL || 'https://your-production-domain.com';
+        return process.env.NEXT_PUBLIC_BASE_URL || 'https://resto-cx-app-fe.vercel.app';
     }
     return 'http://localhost:3000';
 }
