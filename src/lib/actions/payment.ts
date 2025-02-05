7dc7ac619df625672180af163a242a2c487380f1
Windrich <windrich@alvaenergi.com>
2025-02-06 01:05:58 +0800

    upgrade stripe version

--- src/lib/actions/payment.ts
+++ src/lib/actions/payment.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe';
 
 // Initialize Stripe with your secret key
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-01-27.acacia',
 });
 
 export type CreatePaymentIntentParams = {
